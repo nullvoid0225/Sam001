86fd46850f16478c0d6c633090ac09bcd0526c77
nullvoid0225 <51628673+nullvoid0225@users.noreply.github.com>
2019-08-28 01:18:10 +0900

    Update Form1.cs
    Change Button Text

--- Sam001/Form1.cs
+++ Sam001/Form1.cs
@@ -19,7 +19,7 @@ namespace Sam001
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            button1.Text = "Hello";
+            button1.Text = "OK Button";
         }
     }
 }
