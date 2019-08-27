f65200eb42c307ffecc7ef5041efafa9b13d35a7
nullvoid0225 <51628673+nullvoid0225@users.noreply.github.com>
2019-08-28 00:42:04 +0900

    Add From Load Event Handler

--- Sam001/Form1.cs
+++ Sam001/Form1.cs
@@ -16,5 +16,10 @@ namespace Sam001
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Cancel";
+        }
     }
 }
